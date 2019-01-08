9667065bfbbab71ad40ef82ccbd27def20389e04
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-01-08 14:45:53 +0100

    Typo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,16 +81,12 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Numerik Games
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://laravel.com/docs" target="_blank">Documentation</a>
+                    <a href="https://github.com/CPNV-ES/NumerikGames" target="_blank">GitHub</a>
                 </div>
             </div>
         </div>
