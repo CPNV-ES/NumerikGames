26f20ad9b4cf6388a1042735f2f868e379b0ebc5
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-01-24 16:17:22 +0100

    Create Verses seeder

---
+++ database/seeds/VersesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class VersesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
