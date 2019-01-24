df3a9116f99a17768e801c6cb7d568c0ad34c1d5
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-01-24 13:43:33 +0100

    Create database migration

---
+++ database/migrations/2019_01_24_110706_create_verses_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * CreateVersesTable
+ * 
+ * @author Nicolas Henry
+ */
+class CreateVersesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('verses', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('content');
+            $table->string('status');
+            $table->integer('prose_id')->unsigned()->nullable();
+            $table->timestamps();
+
+            // Foreing keys
+            $table->foreign('prose_id')->references('id')->on('proses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('verses', function (Blueprint $table) {
+            $table->dropForeign(['prose_id']);
+        });
+
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists('verses');
+        Schema::enableForeignKeyConstraints();
+    }
+}
