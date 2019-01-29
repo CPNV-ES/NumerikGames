7a0e2b1800194afc5344191a0eb31b2d485fc449
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-01-29 08:57:01 +0100

    Convert route to resouces

---
+++ resources/views/proses/create.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.app')
+@section('content')
+    <div class="flex-center position-ref full-height">
+
+        <div class="content">
+            <div class="title m-b-md">
+                Numerik Games
+            </div>
+
+            <div class="links">
+                <a href="{{url('/')}}" target="_blank">Accueil</a>
+            </div>
+        </div>
+    </div>
+@endsection
