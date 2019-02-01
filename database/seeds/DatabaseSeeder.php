fa615ce4895f086c026104d23c7e25a00cebc02c
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-02-01 14:06:18 +0100

    Create user root

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call([
+            UsersTableSeeder::class,
             ThemesTableSeeder::class,
             ProsesTableSeeder::class,
             VersesTableSeeder::class,
