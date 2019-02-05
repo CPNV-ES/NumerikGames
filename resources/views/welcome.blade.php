f8ba020162730c2f309cf63dfe53a932793f1fc3
Muminovic Anel <Muminovic____@hotmail.ch>
2019-02-05 16:47:10 +0100

    Add grid to themes in home page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,22 @@
                 <h2>Themes page</h2>
             </div>
             <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        @foreach ($themes as $theme)
-                            {{$theme->name}}
-                        @endforeach
-                    </div>
+                <div class="row">                   
+                    @foreach ($themes as $theme)       
+                        <div class="col-sm-3">                
+                            <svg class="bd-placeholder-img figure-img img-fluid rounded" width="400" height="300" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: {{$theme->name}}">
+                                <title>{{$theme->name}}</title>
+                                    <rect fill="#868e96" width="100%" height="100%"></rect>
+                                    <text fill="#dee2e6" dy=".3em" x="50%" y="50%">{{$theme->name}}</text>
+                            </svg>
+                            <a href="{{ route('proses.index', $theme->id) }}">
+                                    <button type="submit" class="btn btn-primary">Voir le contenu</button>
+                            </a>
+                            <a href="{{ route('proses.show', $theme->id) }}">
+                                    <button type="submit" class="btn btn-primary">Ajouter un vers</button>
+                            </a>
+                        </div>
+                    @endforeach                   
                 </div>
             </div>
         </div>
