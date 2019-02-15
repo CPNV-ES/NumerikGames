5d72bfdab5a9adcbdb57b0e87fae181e0fc328e2
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-02-15 11:21:09 +0100

    Add style to prose/id

--- public/js/app.js
+++ public/js/app.js
@@ -36491,8 +36491,8 @@ $(document).ready(function () {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Anel.MUMINOVIC\PhpstormProjects\NumerikGames\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Anel.MUMINOVIC\PhpstormProjects\NumerikGames\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/nawakine/Documents/Ecole/MAW/numerikGames/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /Users/nawakine/Documents/Ecole/MAW/numerikGames/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
