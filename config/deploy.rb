deec7a1059a9a54bb9d2cb810608ed4ef64bd9bc
Jarod Streckeisen <strecky1800@gmail.com>
2019-02-15 11:25:02 +0100

    Add and configure capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,48 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.11.0"
+
+set :application, "NumerikGames"
+set :repo_url, "https://github.com/CPNV-ES/NumerikGames.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, '/var/www/laravel-capistrano'
+set :laravel_dotenv_file, '/var/www/secrets/.env'
+set :keep_releases, 5
+append :linked_dirs,
+    'storage/app',
+    'storage/framework/cache',
+    'storage/framework/sessions',
+    'storage/framework/views',
+    'storage/logs'
+namespace :composer do
+    desc "Running Composer Install"
+    task :install do
+        on roles(:composer) do
+            within release_path do
+                execute :composer, "install --no-dev --quiet --prefer-dist --optimize-autoloader"
+            end
+        end
+    end
+end
+namespace :laravel do
+    task :fix_permission do
+        on roles(:laravel) do
+            execute :chmod, "-R ug+rwx #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
+            execute :chgrp, "-R www-data #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
+        end
+    end
+    task :configure_dot_env do
+    dotenv_file = fetch(:laravel_dotenv_file)
+        on roles (:laravel) do
+        execute :cp, "#{dotenv_file} #{release_path}/.env"
+        end
+    end
+end
+namespace :deploy do
+    after :updated, "composer:install"
+    after :updated, "laravel:fix_permission"
+    after :updated, "laravel:configure_dot_env"
+end
