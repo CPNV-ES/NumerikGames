c7998d15e2d23f6dba90fb82eb0fea426ce9de09
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-02-21 13:59:13 +0100

    Update speed /projectors

--- resources/js/scripts/main.js
+++ resources/js/scripts/main.js
@@ -11,4 +11,20 @@ $(document).ready(function(){
         }
     });
 
+    // Calls the function only for the projectors page
+    if($('div').has('#projectors-index')) {
+       projectorsLoop(); 
+    }
+    
+    // Function that move the proses and verses from bottom to top with an infinite loop
+    function projectorsLoop() {
+        $('nav').css('z-index', 99999);
+        var h_prose = $('.prose').height();
+        $(".prose")
+            .css({top:879,position:'fixed'})        
+            .animate({top: '-'+h_prose}, 130000, 'swing',
+            function () {
+                projectorsLoop()
+            });
+        }
  });
