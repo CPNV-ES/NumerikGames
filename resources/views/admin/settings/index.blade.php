e735a97fdedc74317ac96e93e997cd5133d9b23c
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-04-02 16:15:35 +0200

    Typo

--- resources/views/admin/settings/index.blade.php
+++ resources/views/admin/settings/index.blade.php
@@ -42,39 +42,6 @@
                                             <div class="btn-group btn-group-toggle">
                                                 <a class="btn btn-primary" href="{{ route('admin.settings.edit', $setting) }}" role="button">Editer</a>
                                                 <a class="btn btn-warning" href="{{ route('admin.settings.show', $setting) }}" role="button">Afficher</a>
-                                                
-                                                <!-- Modal -->
-                                                <div class="modal fade" id="exampleModal-{{$setting->id}}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                                    <div class="modal-dialog" role="document">
-                                                        <div class="modal-content">
-                                                            <div class="modal-header">
-                                                                <h5 class="modal-title" id="exampleModalLabel-{{$setting->id}}">Attention</h5>
-                                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                                <span aria-hidden="true">&times;</span>
-                                                                </button>
-                                                            </div>
-                                                            <div class="modal-body">
-                                                                Vous êtes sur le point de supprimer <b>{{$setting->name}}</b>, Veuillez valider votre choix.
-                                                            </div>
-                                                            <div class="modal-footer">
-                                                                <form id="delete-settings-form-{{$setting->id}}"
-                                                                    action="{{ route('admin.settings.destroy', ['setting' => $setting->id]) }}"
-                                                                    method="POST" style="display: none;">
-                                                                    {{ csrf_field() }}
-                                                                    {{ method_field('DELETE') }}
-                                                                </form>
-                                                                <button onclick="event.preventDefault();
-                                                                    document.getElementById('delete-settings-form-{{$setting->id}}').submit();"
-                                                                    class="btn btn-danger">
-                                                                    Supprimer
-                                                                </button>
-                                                            </div> {{-- .modal-footer --}}
-                                                        </div>
-                                                    </div>
-                                                </div> {{-- .modal .fade --}}
-
-                                                <!-- Button trigger modal -->
-                                                <button class="btn btn-danger" data-toggle="modal" data-target="#exampleModal-{{$setting->id}}">Supprimer</button>
                                             </div>
                                         </td>
                                     </tr>   
