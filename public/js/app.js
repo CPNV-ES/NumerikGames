9068eccccc1a2f3877686fdab75eaa693e9e72b9
Muminovic Anel <Muminovic____@hotmail.ch>
2019-04-29 16:00:54 +0200

    Add custom error if empty input

--- public/js/app.js
+++ public/js/app.js
@@ -36520,14 +36520,21 @@ $(document).ready(function () {
     }, 130000, 'swing', function () {
       projectorsLoop();
     });
-  }
+  } // Add the verse from the input to the modal if not empty
+
 
   $('#addVerse').on('click', function () {
     var verse = $('#verse').val();
-    console.log(verse);
-    var modal = $('#exampleModalCenter');
-    modal.find('.modal-body #modalVerse').text(verse);
-    modal.find('.modal-body #verse').val(verse);
+
+    if (!verse) {
+      $('#error').addClass("alert alert-danger alert-block");
+      $('#error strong').html("Vous n'avez pas spécifié de vers !");
+    } else {
+      var modal = $('#exampleModalCenter');
+      modal.modal("show");
+      modal.find('.modal-body #modalVerse').text(verse);
+      modal.find('.modal-body #verse').val(verse);
+    }
   });
 });
 
