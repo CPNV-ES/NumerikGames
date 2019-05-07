2dc443562bd0fc331772f590b7d39e5e9c3a55f6
JarodS <strecky1800@gmail.com>
2019-05-07 16:54:36 +0200

    Update Cap files to fit swisscenter

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,87 +1,39 @@
 # config valid for current version and patch releases of Capistrano
-# lock "~> 3.11.0"
+#lock "~> 3.11.0"
 
-set :application, "NumerikGames"
-set :repo_url, "git@github.com:CPNV-ES/NumerikGames.git"
-
-set :deploy_to, '/var/www/numerikgames/deploy'
-
-set :ssh_options, {
-  :forward_agent => false
-}
+#set :application, "my_app_name"
+#set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
-#set :branch, "develop@v2"
-# Default deploy_to directory is /var/www/laravel-capistrano
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-set :laravel_dotenv_file, '/var/www/secret/.env'
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
 # Default value for keep_releases is 5
-set :keep_releases, 5
+# set :keep_releases, 5
 
-append :linked_dirs,
-   'storage/app/public/pictures/themes'
-
-namespace :composer do
-   desc "Running Composer Install"
-   task :install do
-       on roles(:app) do
-           within release_path do
-               execute :composer, "install"
-               execute :composer, "dump-autoload"
-           end
-       end
-   end
-end
-
-namespace :laravel do
-   task :fix_permission do
-       on roles(:laravel) do
-           execute :chmod, "-R ug+rwx #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
-           execute :chmod, "-R 777 #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
-           #execute :chgrp, "-R www-data #{shared_path}/storage/ #{release_path}/bootstrap/cache/"
-       end
-   end
-   task :configure_dot_env do
-   dotenv_file = fetch(:laravel_dotenv_file)
-       on roles (:laravel) do
-       execute :cp, "#{dotenv_file} #{release_path}/.env"
-       end
-   end
-   task :key do
-       on roles(:all) do
-           within release_path do
-                execute "php", "artisan", "key:generate"
-           end
-       end
-   end
-   task :mix do
-       on roles(:all) do
-           within release_path do
-               execute "npm", "install"
-                execute "npm", "run", "prod"
-           end
-       end
-   end
-   task :database do
-       on roles(:all) do
-           within release_path do
-                execute "php", "artisan", "migrate:fresh --seed"
-           end
-       end
-   end
-   task :link do
-       on roles(:all) do
-           within release_path do
-                execute "php", "artisan", "storage:link"
-           end
-       end
-   end
-end
-namespace :deploy do
-   after :updated, "composer:install"
-   after :updated, "laravel:fix_permission"
-   after :updated, "laravel:configure_dot_env"
-   after :updated, "laravel:key"
-   after :updated, "laravel:database"
-   after :updated, "laravel:link"
-end
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
