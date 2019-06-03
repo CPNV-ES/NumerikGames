45443551eaa2f9d4ef67d478dad229742d607d1f
Nicolas Henry <nicolas.henry@cpnv.ch>
2019-06-03 14:08:01 +0200

    Add settings for game

--- resources/views/proses/show.blade.php
+++ resources/views/proses/show.blade.php
@@ -37,6 +37,7 @@
             </div>
         </div>
     </div>
+    
     <!-- Modal -->
     <div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
