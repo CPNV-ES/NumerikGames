3be5a66e61ef471b06ba5f6accfe8be80bba17dc
JarodS <strecky1800@gmail.com>
2019-06-05 11:06:00 +0200

    Added app file (controller /model

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,25 +3,35 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-
+use Illuminate\Support\Facades\Schema;
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
+    {
+        /* Add for log the sql on /storage/logs/ */
+        Schema::defaultStringLength(191);  // <== Mandatory for production use
+        if (\App::environment('local')) {
+            \DB::listen(function($query) {
+                \Log::info(
+                    $query->sql,
+                    $query->bindings,
+                    $query->time
+                );
+            });
+        }
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
         //
     }
