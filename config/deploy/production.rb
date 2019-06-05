85044400eb2c13eedabc254fcadda4aef7eb6200
JarodS <strecky1800@gmail.com>
2019-06-05 10:40:47 +0200

    Added verses route and everything to deploy

---
+++ config/deploy/production.rb
@@ -0,0 +1,6 @@
+set :swisscenter_username, "numgames"
+set :swisscenter_servername, "numerikgames.mycpnv.ch"
+
+set :repo_url, "git@github.com:CPNV-ES/NumerikGames.git"
+set :branch ,"feature/test_production"
+require_relative 'laravel_swisscenter'
